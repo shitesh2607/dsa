be54781b225027ece7569d2916b6d22eb9716a48
shitesh2607 <shitesh2607@gmail.com>
2021-11-27 13:42:49 +0530

    K-th element of two sorted Arrays using binary search

--- Binary_search/K-th_element_of_two_sorted_Arrays.cpp
+++ Binary_search/K-th_element_of_two_sorted_Arrays.cpp
@@ -1,1 +1,9 @@
-// ?hitesh Gu[ptta]
+#include<bits/stdc++.h>
+using namespace std;
+class solution{
+    public:
+    int kth_Element(vector<int>nums1, vector<int>nums2, int k){
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+    }
+};
