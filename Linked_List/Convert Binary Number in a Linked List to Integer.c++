e517abef909a58609ee3f9f8c8c4ab3ec5073c2d
shitesh2607 <shitesh2607@gmail.com>
2021-12-07 15:14:30 +0530

    Convert Binary Number in a Linked List to Integer

---
+++ Linked_List/Convert Binary Number in a Linked List to Integer.c++
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode
+{
+    /* data */
+    int val;
+    ListNode* next;
+};
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int ans=0;
+        ListNode *temp;
+        temp = head;
+        int i=0;
+        int temp_i=0;
+        while(temp != NULL){
+            if(i>temp_i){
+                ans = ans*2;
+            }
+            ans = ans + temp->val * pow(2,0);
+            temp = temp->next;
+            temp_i = i;
+            i++;
+        }
+        // or
+        while(temp != NULL){
+            ans*=2;
+            ans+=(temp->val);
+            temp = temp->next;
+        }
+
+        return ans;
+    }
+};
---
+++ Linked_List/middle_of-linked_list.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode 
+{
+    /* data */
+    int val;
+    ListNode* next;
+};
+class solution{
+    public:
+    // solution 1
+    ListNode* middleNode(ListNode* head){
+        int n=0;
+        ListNode* temp = head;
+        while(temp != NULL){
+            temp = temp->next;
+            n++;
+        }
+        int half = n/2;
+        temp = head;
+        while (half--)
+        {
+            temp = temp->next;
+        }
+        return temp;
+    }
+    // optimiized solution 2
+    ListNode* solution2(ListNode* head){
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
